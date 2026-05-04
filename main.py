98aebef5aacf4e402d78f722be9c02a43dba1bde
00001141031346 <00001141031346@al.educacao.sp.gov.br>
2026-05-04 12:17:07 -0300

    adiciona menu

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+while True:
+    print("Sistema de Reserva de Assentos")
+
+    print("1. Usuários")
+    print("2. Assentos")
+    print("3. Sair")
+
+
+    opcao = input("Esolha o assento dispnivel: ")
