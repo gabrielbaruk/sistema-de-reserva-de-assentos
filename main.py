2ed67e801af7493a6d2a3a8174ca33cdad416f48
00001141031346 <00001141031346@al.educacao.sp.gov.br>
2026-05-14 12:11:04 -0300

    Adiciona submenu

--- main.py
+++ main.py
@@ -8,10 +8,30 @@ while True:
     opcao = (input("Escolha umas da opções: "))
 
     if opcao == "1":
-        print("Usuários")
+        print("1. Usuários")
+        print("2. Ver usuário")
+        print("3. Verificar")
+        opcao = input("Escolha: ")
+        if opcao == "1":
+            print("Cadastrar usuário")
+            Digite_um_nome = input("Digite seu nome: ")
+        elif opcao == "2":
+            print("Listar Usuário")
+        elif opcao == "3":
+            print("Verificar usuário")
+            Faça_uma_verificação = input("Resolva O ReCaptcha")
+        else:
+            print("Opção inválida. Por favor tente novamente")
+            continue
     elif opcao == "2":
-        print("Assentos")
+        print("1. Assentos")
+        opcao = input("Escolha: ")
+        if opcao == "1":
+            print("Mostrar mapa")
+            Digite_seu_assento = input("Qual seu assento? ")
+            print("Mostrar assento")
     elif opcao == "3":
+        print("Saindo do sistema...")
         break
     else:
         print("Opcão inválida")
